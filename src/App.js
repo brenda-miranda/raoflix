16b0c494275cc628e2ef2f42ab22232efe7d6f43
Brenda Miranda <brendamndx@gmail.com>
2020-07-27 16:44:04 -0300

    Fix errors in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Menu from './components/Menu'
+import Menu from './components/Menu';
 import dadosIniciais from './data/dados_iniciais.json';
 import BannerMain from './components/BannerMain';
 import Carousel from './components/Carousel';
