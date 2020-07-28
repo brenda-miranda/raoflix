af72e15522fae25a794ee70b0c7e7b5cb1306263
Brenda Miranda <brendamndx@gmail.com>
2020-07-28 16:59:08 -0300

    Change slider dots to infinite: true

--- src/components/Carousel/components/Slider/index.js
+++ src/components/Carousel/components/Slider/index.js
@@ -42,7 +42,7 @@ const Slider = ({ children }) => (
   <Container>
     <SlickSlider {...{
       dots: false,
-      infinite: false,
+      infinite: true,
       speed: 300,
       centerMode: false,
       variableWidth: true,
