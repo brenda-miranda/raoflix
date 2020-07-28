3a2ba103175fab58bca0bfb7077ce111fdd41e08
Brenda Miranda <brendamndx@gmail.com>
2020-07-28 16:40:12 -0300

    Add styles to Main in Layout

---
+++ src/components/Layout/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Main = styled.main`
+   background: var(--black);
+   color: var(--white);
+   flex: 1;
+   padding-top: 50px;
+   padding-left: 5%;
+   padding-right: 5%;
+`;
