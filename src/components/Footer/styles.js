75c41c479d9f40ab09ff0ec6724ad1274fbabc94
Brenda Miranda <brendamndx@gmail.com>
2020-07-28 17:55:41 -0300

    Change background color

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 
 export const FooterBase = styled.footer`
-  background: var(--black);
+  background: #0a0a0a;
   border-top: 2px solid var(--primary);
   padding-left: 16px;
   padding-right: 16px;
