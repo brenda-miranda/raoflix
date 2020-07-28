3f9076365216ca2a64b234b117e29f7c8ac3bee9
Brenda Miranda <brendamndx@gmail.com>
2020-07-28 17:55:17 -0300

    Change linear-gradient background color of BannerMain

--- src/components/BannerMain/styles.js
+++ src/components/BannerMain/styles.js
@@ -96,7 +96,7 @@ export const BannerMainContainer = styled.section`
 
   &:after {
     bottom: 0;
-    background: linear-gradient(0deg, #141414 0%, transparent 100%);
+    background: linear-gradient(0deg, #0a0a0a 0%, transparent 100%);
   }
 `;
 
