85832566102078c88150f5afb963ecd1ec1cadf0
Brenda Miranda <brendamndx@gmail.com>
2020-07-28 16:38:46 -0300

    Add a new route to /cadastro/categoria

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import Home from './pages/Home';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import CadastroVideo from './pages/Cadastro/Video';
+import CadastroCategoria from './pages/Cadastro/Categoria';
 
 const Page404 = () => (<div>Página 404</div>)
 
@@ -12,7 +13,7 @@ ReactDOM.render(
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/cadastro/video" component={CadastroVideo} />
-      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
       <Route component={Page404} />
     </Switch>
   </BrowserRouter>,
