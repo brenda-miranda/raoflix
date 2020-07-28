8117a3ecb3dd82a494153e9405d1f4b20e48e49f
Brenda Miranda <brendamndx@gmail.com>
2020-07-28 15:28:47 -0300

    Create a new Layout component

---
+++ src/pages/Cadastro/Video/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../../../components/Layout';
+
+function CadastroVideo() {
+   return (
+      <Layout>
+         Página de Cadastro de Vídeo
+      </Layout>
+   )
+}
+
+export default CadastroVideo;
