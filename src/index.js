0cd5f07df1e810eaaf00050f16539a981c657f05
Brenda Miranda <brendamndx@gmail.com>
2020-07-28 15:26:37 -0300

    Create new routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/Cadastro/Video';
+
+const Page404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route component={Page404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
