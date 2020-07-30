5d083d72b671b75cb38ea539e491ee80665e5225
Brenda Miranda <brendamndx@gmail.com>
2020-07-30 18:08:21 -0300

    Add scroll effect to menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/Logo.png';
 import './styles.css';
 import Button from '../Button';
-// import ButtonLink from './components/ButtonLink';
 
-function Menu() {
+const Menu = () => {
+  const [noHeaderBackground, setNoHeaderBackground] = useState(true)
+  const [headerHidden] = useState(false)
+  useEffect(() => {
+    window.addEventListener('scroll', changeHeaderBackground, false)
+  }, [])
+
+  const changeHeaderBackground = () => {
+    if (window.scrollY > 0) {
+      setNoHeaderBackground(false)
+    } else {
+      setNoHeaderBackground(true)
+    }
+  }
+
   return (
-    <nav className="Menu">
-      <Link to="/">
-        <img className="Logo" src={Logo} alt="AluraFlix logo" />
-      </Link>
+    <>
+      <header
+        className={`Menu ${
+          (noHeaderBackground && 'no-header-background') || 'header-background'
+        } ${(headerHidden && 'header-hidden') || ''}
+        }`}
+      >
+       <nav className="Menu">
+            <Link to ="/">
+                <img className="Logo" src={Logo} alt="EnglishFlix"/>
+            </Link>
 
-      <Button as={Link} className="ButtonLink" to="/cadastro/video">
-        Novo vídeo
-      </Button>
-    </nav>
-  );
+            <Button as={Link} className="ButtonLink" to ="/cadastro/video">
+                Novo vídeo
+            </Button>
+        </nav>
+      </header>
+    </>
+  )
 }
 
-export default Menu;
+export default Menu
